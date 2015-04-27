ecb494fd7974b178702c3454ed8cb17c6f90eabc
Chung-Heng Yeh <chungheng.yeh@gmail.com>
2015-04-27 15:05:51 -0400

    Restructured files

--- flyor/baseormap.py
+++ flyor/baseormap.py
@@ -77,26 +77,6 @@ class BaseORMap(object):
             c = item
         return [c]*self.n
 
-class HallemORMap(BaseORMap):
-    """
-    Hallem and Carlson Dataset
-    """
-    def __init__(self,filename):
-        self._ordict = {}
-        with open(filename) as f:
-            self.gl = f.readline().split('\t')[1:]
-            self.osn = f.readline().split('\t')[1:]
-            for line in f:
-                seg = line.split('\t')
-                self._ordict[seg[0]] = np.array([float(x) for x in seg[1:]])
-        super(HallemORMap,self).__init__(len(self._ordict))
-
-    def __getitem__(self,item):
-        c = item[0]
-        return (1.-c)*self._ordict['spontaneous firing rate'] + \
-               c*self._ordict[item[1]]
-
-
 if __name__ == "__main__":
     B = BaseORMap(5)
     print B[3.]
